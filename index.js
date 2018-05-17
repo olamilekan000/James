92885cc7651485bb360cd52355cb3b21fe98bae9
Ola <olarpringo@gmail.com>
2018-05-17 03:10:58 +0100

    not ok

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
 
 //routes
@@ -17,21 +17,21 @@ app.get('/', (req, res) => {
 	res.send('I am a bot');
 })
 
-//facebook
+// facebook
 
-// app.get('/', (req, res) => {
-// 	const hubChallenge = req.query[`hub.challenge`];
-// 	const hubMode = req.query[`hub.mode`];
+app.get('/', (req, res) => {
+	const hubChallenge = req.query[`hub.challenge`];
+	const hubMode = req.query[`hub.mode`];
 
-// 	const verifyTokenMatches = (req.query[`hub.verify_token`] === `fess`);
+	const verifyTokenMatches = (req.query[`hub.verify_token`] === `fess`);
 
-// 	if(verifyTokenMatches && hubMode){
-// 		res.status(200).send(hubChallenge)
-// 	}else{
-// 		res.status(403).end();
-// 	}
-// })
+	if(verifyTokenMatches && hubMode){
+		res.status(200).send(hubChallenge)
+	}else{
+		res.status(403).end();
+	}
+})
 
-app.listen(app.get('port'), () =>{
+app.listen(app.get('port'), () => {
 	console.log('listening for requests...')
 })
