aea6044dbf13db9edb18249fa37e63e4b7e1ea13
Olamilekan Odukoya <olarpringo@gmail.com>
2018-06-10 11:48:00 +0100

    fb hook 22

--- index.js
+++ index.js
@@ -74,15 +74,15 @@ app.post('/webhook', (req, res) => {
 							    {
 										     	
 								  	"text": "Objective of IFRS 1",
-								  	"postback": "Objective of IFRS 1"
+								  	"postback": "Objective"
 								 },
 								 {
 								 	"text": "Measurement OF IFRS 1",
-								  	"postback": "Measurement OF IFRS 1"
+								  	"postback": "Measurement"
 								 },
 								 {
 								 	"text": "Disclosure of IFRS 1",
-								  	"postback": "Disclosure of IFRS 1"
+								  	"postback": "Disclosure"
 								 }
 							  ]
 							}
@@ -93,6 +93,26 @@ app.post('/webhook', (req, res) => {
 	}
 
 });
+
+// {
+//     'platform': 'ACTIONS_ON_GOOGLE',
+//     'basic_card': {
+//       'title': 'Title: this is a title',
+//       'subtitle': 'This is an subtitle.',
+//       'formatted_text': 'Body text can include unicode characters including emoji 📱.',
+//       'image': {
+//         'image_uri': 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png'
+//       },
+//       'buttons': [
+//         {
+//           'title': 'This is a button',
+//           'open_uri_action': {
+//             'uri': 'https://assistant.google.com/'
+//           }
+//         }
+//       ]
+//     }
+//   }
 
 const richResponseV2Card = {
   'title': 'Title: this is a title',
