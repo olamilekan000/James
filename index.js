38325856fe8d0c198770732a6699a33e5d4f1ac0
Olamilekan Odukoya <olarpringo@gmail.com>
2018-06-11 00:45:58 +0100

    fb hook 25

--- index.js
+++ index.js
@@ -66,29 +66,20 @@ app.post('/webhook', (req, res) => {
 			res.json({
 				"fulfillmentMessages": [
 					{
-						"card": {
-							"title": "IFRS 1",
-							"subtitle": "IFRS 1 First-time Adoption of International Financial Reporting Standards.",
-							"imageUri": "https://www.secto.fi/wp-content/uploads/IFRS-2-480x320.jpg",
-							"buttons": [
-							    {     	
-								  	"text": "Objective of IFRS 1",
-								  	"postback": "Objective"
-								 },
-								 {
-								 	"text": "Measurement OF IFRS 1",
-								  	"postback": "Measurement"
-								 },
-								 {
-								 	"text": "Disclosure of IFRS 1",
-								  	"postback": "Disclosure"
-								 },
-								 {
-								 	"text": "Read more",
-								 	"postback": "https://www.iasplus.com/en/standards/ifrs/ifrs1"
-								 }
-							  ]
-							}
+						"quickReplies": {
+							"title": "IFRS 1 First-time Adoption of International Financial Reporting Standards sets \
+							out the procedures that an entity must follow when it adopts IFRSs for the first time as \
+							the basis for preparing its general purpose financial statements. The IFRS grants limited\
+							exemptions from the general requirement to comply with each IFRS effective at the end of \
+							its first IFRS reporting period.\
+							A restructured version of IFRS 1 was issued in November 2008 and applies if an entity's first\
+							IFRS financial statements are for a period beginning on or after 1 July 2009.",
+							"quickReplies": [
+								"Objective of IFRS 1",
+								"measurement of IFRS 1",
+								"Disclosure of IFRS 1"
+							]
+						}
 
 					}
 				]
@@ -188,3 +179,7 @@ app.listen(process.env.PORT || PORT, () => {
 });
 
 // originalDetectIntentRequest
+
+	// "imageUri": "https://www.secto.fi/wp-content/uploads/IFRS-2-480x320.jpg",
+	
+	// "postback": "https://www.iasplus.com/en/standards/ifrs/ifrs1"
