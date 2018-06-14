18471626585459c7d14907b3c8e6828c1ec5a732
Olamilekan Odukoya <olarpringo@gmail.com>
2018-06-14 03:35:18 +0100

    separated IAS and IFRS

--- standards/allStandards.js
+++ standards/allStandards.js
@@ -279,48 +279,48 @@ module.exports = (req, res) => {
 							]
 						}
 					},
-					{
-						"card":{
-							"title": "IFRS 14 — Regulatory Deferral Accounts",
-							"subtitle": "IFRS 14 was originally issued in January 2014 and applies to an entity's first annual IFRS financial statements for a period beginning on or after 1 January 2016.",
-							"imageUri": textz.imgeUri,
-							"buttons":[
-								{
-									'text': 'Objective of IFRS 14',
-	      							'postback': 'Objective of IFRS 14'
-								},
-								{
-									'text': 'Measurement of IFRS 14',
-									'postback': 'Measurement of IFRS 14'
-								},
-								{
-									'text': 'Disclosure of IFRS 14',
-									'postback': 'Disclosure of IFRS 14'
-								}
-							]
-						}
-					},
-					{
-						"card":{
-							"title": "IFRS 15 — Revenue from Contracts with Customers",
-							"subtitle": "IFRS 15 was issued in May 2014 and applies to an annual reporting period beginning on or after 1 January 2018. On 12 April 2016, clarifying amendments were issued that have the same effective date as the standard itself.",
-							"imageUri": textz.imgeUri,
-							"buttons":[
-								{
-									'text': 'Objective of IFRS 15',
-	      							'postback': 'Objective of IFRS 15'
-								},
-								{
-									'text': 'Measurement of IFRS 15',
-									'postback': 'Measurement of IFRS 15'
-								},
-								{
-									'text': 'Disclosure of IFRS 15',
-									'postback': 'Disclosure of IFRS 15'
-								}
-							]
-						}
-					}
+					// {
+					// 	"card":{
+					// 		"title": "IFRS 14 — Regulatory Deferral Accounts",
+					// 		"subtitle": "IFRS 14 was originally issued in January 2014 and applies to an entity's first annual IFRS financial statements for a period beginning on or after 1 January 2016.",
+					// 		"imageUri": textz.imgeUri,
+					// 		"buttons":[
+					// 			{
+					// 				'text': 'Objective of IFRS 14',
+	    //   							'postback': 'Objective of IFRS 14'
+					// 			},
+					// 			{
+					// 				'text': 'Measurement of IFRS 14',
+					// 				'postback': 'Measurement of IFRS 14'
+					// 			},
+					// 			{
+					// 				'text': 'Disclosure of IFRS 14',
+					// 				'postback': 'Disclosure of IFRS 14'
+					// 			}
+					// 		]
+					// 	}
+					// },
+					// {
+					// 	"card":{
+					// 		"title": "IFRS 15 — Revenue from Contracts with Customers",
+					// 		"subtitle": "IFRS 15 was issued in May 2014 and applies to an annual reporting period beginning on or after 1 January 2018. On 12 April 2016, clarifying amendments were issued that have the same effective date as the standard itself.",
+					// 		"imageUri": textz.imgeUri,
+					// 		"buttons":[
+					// 			{
+					// 				'text': 'Objective of IFRS 15',
+	    //   							'postback': 'Objective of IFRS 15'
+					// 			},
+					// 			{
+					// 				'text': 'Measurement of IFRS 15',
+					// 				'postback': 'Measurement of IFRS 15'
+					// 			},
+					// 			{
+					// 				'text': 'Disclosure of IFRS 15',
+					// 				'postback': 'Disclosure of IFRS 15'
+					// 			}
+					// 		]
+					// 	}
+					// }
 					// 					{
 					// 	"card":{
 					// 		"title": "IFRS 16 — Leases",
