3e3588ae872fd754b304f83141206d7d42c7b0af
Olamilekan Odukoya <olarpringo@gmail.com>
2018-06-14 03:27:13 +0100

    separated IAS and IFRS

--- standards/allStandards.js
+++ standards/allStandards.js
@@ -282,20 +282,20 @@ module.exports = (req, res) => {
 					{
 						"card":{
 							"title": "IFRS 14 — Regulatory Deferral Accounts",
-							"subtitle": "IFRS 10 was issued in May 2011 and applies to annual periods beginning on or after 1 January 2013.",
+							"subtitle": "IFRS 14 was originally issued in January 2014 and applies to an entity's first annual IFRS financial statements for a period beginning on or after 1 January 2016.",
 							"imageUri": textz.imgeUri,
 							"buttons":[
 								{
-									'text': 'Objective of IFRS 10',
-	      							'postback': 'Objective of IFRS 10'
+									'text': 'Objective of IFRS 14',
+	      							'postback': 'Objective of IFRS 14'
 								},
 								{
-									'text': 'Measurement of IFRS 10',
-									'postback': 'Measurement of IFRS 10'
+									'text': 'Measurement of IFRS 14',
+									'postback': 'Measurement of IFRS 14'
 								},
 								{
-									'text': 'Disclosure of IFRS 10',
-									'postback': 'Disclosure of IFRS 10'
+									'text': 'Disclosure of IFRS 14',
+									'postback': 'Disclosure of IFRS 14'
 								}
 							]
 						}
@@ -303,66 +303,66 @@ module.exports = (req, res) => {
 					{
 						"card":{
 							"title": "IFRS 15 — Revenue from Contracts with Customers",
-							"subtitle": "IFRS 10 was issued in May 2011 and applies to annual periods beginning on or after 1 January 2013.",
+							"subtitle": "IFRS 15 was issued in May 2014 and applies to an annual reporting period beginning on or after 1 January 2018. On 12 April 2016, clarifying amendments were issued that have the same effective date as the standard itself.",
 							"imageUri": textz.imgeUri,
 							"buttons":[
 								{
-									'text': 'Objective of IFRS 10',
-	      							'postback': 'Objective of IFRS 10'
+									'text': 'Objective of IFRS 15',
+	      							'postback': 'Objective of IFRS 15'
 								},
 								{
-									'text': 'Measurement of IFRS 10',
-									'postback': 'Measurement of IFRS 10'
+									'text': 'Measurement of IFRS 15',
+									'postback': 'Measurement of IFRS 15'
 								},
 								{
-									'text': 'Disclosure of IFRS 10',
-									'postback': 'Disclosure of IFRS 10'
+									'text': 'Disclosure of IFRS 15',
+									'postback': 'Disclosure of IFRS 15'
 								}
 							]
 						}
-					},
-										{
-						"card":{
-							"title": "IFRS 16 — Leases",
-							"subtitle": "IFRS 10 was issued in May 2011 and applies to annual periods beginning on or after 1 January 2013.",
-							"imageUri": textz.imgeUri,
-							"buttons":[
-								{
-									'text': 'Objective of IFRS 10',
-	      							'postback': 'Objective of IFRS 10'
-								},
-								{
-									'text': 'Measurement of IFRS 10',
-									'postback': 'Measurement of IFRS 10'
-								},
-								{
-									'text': 'Disclosure of IFRS 10',
-									'postback': 'Disclosure of IFRS 10'
-								}
-							]
-						}
-					},
-					{
-						"card":{
-							"title": "IFRS 17 — Insurance Contracts",
-							"subtitle": "IFRS 10 was issued in May 2011 and applies to annual periods beginning on or after 1 January 2013.",
-							"imageUri": textz.imgeUri,
-							"buttons":[
-								{
-									'text': 'Objective of IFRS 10',
-	      							'postback': 'Objective of IFRS 10'
-								},
-								{
-									'text': 'Measurement of IFRS 10',
-									'postback': 'Measurement of IFRS 10'
-								},
-								{
-									'text': 'Disclosure of IFRS 10',
-									'postback': 'Disclosure of IFRS 10'
-								}
-							]
-						}
-					}
+					// },
+					// 					{
+					// 	"card":{
+					// 		"title": "IFRS 16 — Leases",
+					// 		"subtitle": "IFRS 16 was issued in January 2016 and applies to annual reporting periods beginning on or after 1 January 2019.",
+					// 		"imageUri": textz.imgeUri,
+					// 		"buttons":[
+					// 			{
+					// 				'text': 'Objective of IFRS 16',
+	    //   							'postback': 'Objective of IFRS 16'
+					// 			},
+					// 			{
+					// 				'text': 'Measurement of IFRS 16',
+					// 				'postback': 'Measurement of IFRS 16'
+					// 			},
+					// 			{
+					// 				'text': 'Disclosure of IFRS 16',
+					// 				'postback': 'Disclosure of IFRS 16'
+					// 			}
+					// 		]
+					// 	}
+					// },
+					// {
+					// 	"card":{
+					// 		"title": "IFRS 17 — Insurance Contracts",
+					// 		"subtitle": "IFRS 17 was issued in May 2017 and applies to annual reporting periods beginning on or after 1 January 2021.",
+					// 		"imageUri": textz.imgeUri,
+					// 		"buttons":[
+					// 			{
+					// 				'text': 'Objective of IFRS 17',
+	    //   							'postback': 'Objective of IFRS 17'
+					// 			},
+					// 			{
+					// 				'text': 'Measurement of IFRS 17',
+					// 				'postback': 'Measurement of IFRS 17'
+					// 			},
+					// 			{
+					// 				'text': 'Disclosure of IFRS 17',
+					// 				'postback': 'Disclosure of IFRS 17'
+					// 			}
+					// 		]
+					// 	}
+					// }
 																																																																																																																						
 				]
 			}
