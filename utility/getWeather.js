bd6f8ced05995b52afffe335cb668f9d57d30c23
Olamilekan Odukoya <olarpringo@gmail.com>
2018-06-21 22:55:10 +0100

    added .env

--- utility/getWeather.js
+++ utility/getWeather.js
@@ -1,4 +1,5 @@
 const request = require('request');
+require('dotenv').config();
 
 module.exports = (req, res) => {
 
@@ -37,7 +38,7 @@ module.exports = (req, res) => {
 //weather api function
 var getState = (state) => {
 		return new Promise((resolve, reject) => {
-			const apiKey = '1545ad0038b38ead324bfab9e11bb464';
+			const apiKey = process.env.WEATHER_SECRET_KEY;
 			const url = `http://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${apiKey}`;
 
 			request(url, (err, response, body) => {
