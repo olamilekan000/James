93d3c127953f2109ad903fa058ca517ab40b41c2
Olamilekan Odukoya <olarpringo@gmail.com>
2018-06-21 23:00:19 +0100

    added .env

--- utility/getWeather.js
+++ utility/getWeather.js
@@ -38,7 +38,7 @@ module.exports = (req, res) => {
 //weather api function
 var getState = (state) => {
 		return new Promise((resolve, reject) => {
-			const apiKey = process.env.WEATHER_SECRET_KEY;
+			const apiKey = process.env.WEATHER_SECRET_KEY
 			const url = `http://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${apiKey}`;
 
 			request(url, (err, response, body) => {
