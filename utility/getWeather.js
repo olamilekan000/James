91bb3cd7d776da544d3edf3e4426db82a930ca69
Olamilekan Odukoya <olarpringo@gmail.com>
2018-06-22 02:47:31 +0100

    added .env 3

--- utility/getWeather.js
+++ utility/getWeather.js
@@ -33,9 +33,10 @@ module.exports = (req, res) => {
 //weather api function
 var getState = (state) => {
 		return new Promise((resolve, reject) => {
+			console.log(process.env.WEATHERSECRET_KEY)
 			const apiKey = process.env.WEATHERSECRET_KEY;
 			const url = `http://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${apiKey}`;
-
+			
 			request(url, (err, response, body) => {
 				if(err) console.log(err)
 
@@ -43,18 +44,17 @@ var getState = (state) => {
 				if (wether.cod == 200 ) {
 					description = "it's " + wether.weather[0].description + " in " + wether.name;
 					resolve(description);
+					console.log(description)
 				}else if(wether.cod == 400 ){
 					description = "city not found";
 					resolve(description);
+					
 				}
 			});
+			
 		});
 	}
 
-var ska = () => {
-	console.log(process.env.WEATHERSECRET_KEY)
-}
-
-ska();
+getState('Abuja')
 
 
