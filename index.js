5785f914fb0b6479085fad11155ac79b342b88a1
Olamilekan Odukoya <olarpringo@gmail.com>
2018-06-26 04:55:02 +0100

    adjusted webhook2 for remaining IAS standards 2

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const wholeStandards = require('./standards/allStandards'); //IFRS 1 - 9
 const allStandardsIFRSCont = require('./standards/allStndardsIfrsContd'); //IFRS 10 - 17
 const allStandardsIAS = require('./standards/allStandardsIas'); //IAS 1 - 17
 const allStandardsIAScont = require('./standards/allStandardsIas2'); //IAS 20 - 30
+const allStandardsIAScont2 = require('./standards/allStandardsIas3'); //IAS 31 - 41
 
 //WEBHOOKS FOR EACH STANDARDS
 const ifrs1WebHook = require('./standards/ifrs1');
@@ -42,6 +43,8 @@ app.post('/webhook', (req, res) => {
 	allStandardsIAS(req, res);
 	//fires the fuction for the remaining IAS standards
 	allStandardsIAScont(req, res);
+	//fires the fuction for the remaining IAS standards
+	allStandardsIAScont2(req, res);
 
 });
 
