86773f95def26249705ee63a3ca48ef0e0ad1c1c
Olamilekan Odukoya <olarpringo@gmail.com>
2018-07-06 04:29:36 +0100

    included user name in welcome

--- utility/welcome.js
+++ utility/welcome.js
@@ -1,28 +1,43 @@
-module.exports = (req, res) => {
+const request = require('request');
+require('dotenv').config();
 
+module.exports = (req, res) => {
 	if(req.body.queryResult.intent.displayName === "Default Welcome Intent"){
-		res.json({
-			"fulfillmentMessages": [{
-				"quickReplies": {
-					  "title": `Hi {{user_first_name}}! What are the things you want to hear abou?`,
-					  "quickReplies": [
-					    "The History of IFRS",
-					    "Conceptul Framework",
-					    "IAS",
-					    "IFRS"
-					]
-				}
-			}]			
+		let Uname = ""
+		getName().then(() => {
+			res.json({
+				"fulfillmentMessages": [{
+					"quickReplies": {
+						  "title": `Hi ${Uname} What are the things you want to hear abou?`,
+						  "quickReplies": [
+						    "The History of IFRS",
+						    "Conceptul Framework",
+						    "IAS",
+						    "IFRS"
+						]
+					}
+				}]			
+			})
 		})
 	}
 }
 
-	// request({
- // 		url: 'https://graph.facebook.com/v2.6/me/messages',
-	// 	qs: { access_token: process.env.FB_PAGE_ACCESS_TOKEN },
-	// 	method: `POST`,
-	// 	json: {
-	// 		recipient: { id: senderId },
-	// 		message: { text },
-	// 	}
-	// });
+var getName = () => {
+		return new Promise((resolve, reject) => {
+
+			let apiKey = process.env.FB_PAGE_ACCESS_TOKEN;
+			console.log(apiKey)
+			const url = `https://graph.facebook.com/v2.6/me?fields=id,name&access_token=${apiKey}`;
+
+			request(url, (err, response, body) => {
+				if(err) console.log(err)
+
+				let Uname = JSON.parse(body)
+				resolve(Uname)
+				console.log(Uname.name)
+			});
+			
+		});
+	}
+
+// getName();	
