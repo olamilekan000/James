5fdbc7ea09498a1c3ab616355223e356166516e9
Olamilekan Odukoya <olarpringo@gmail.com>
2018-07-21 04:04:54 +0100

    added emoji

--- utility/welcome.js
+++ utility/welcome.js
@@ -8,7 +8,7 @@ module.exports = (req, res) => {
 			res.json({
 				"fulfillmentMessages": [{
 					"quickReplies": {
-						  "title": `Hi What are the things you want to know about?`,
+						  "title": `Hi What are the things you want to know about? 😃`,
 						  "quickReplies": [
 						    "The History of IFRS",
 						    "Conceptul Framework",
@@ -22,25 +22,3 @@ module.exports = (req, res) => {
 	}
 }
 
-//gets the name of the user
-// var getName = () => {
-// 	return new Promise((resolve, reject) => {
-// 	let Uname
-// 	let apiKey = process.env.FB_PAGE_ACCESS_TOKEN;
-// 	console.log(apiKey)
-// 	const url = `https://graph.facebook.com/v2.6/me?fields=name&access_token=${apiKey}`;
-
-// 	request(url, (err, response, body) => {
-// 		if(err) console.log(err)
-
-// 		let Uname = JSON.parse(body)
-// 		let Unewname = Uname.name
-// 		resolve(Unewname)
-// 		console.log(Unewname)
-// 	});
-
-// 	});
-// }
-
-
-// getName();	
