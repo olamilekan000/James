03c07619aeeda5a62e167fd2b6a78891d1c893aa
Olamilekan Odukoya <olarpringo@gmail.com>
2018-08-09 02:05:37 +0100

    error function

--- utility/welcome.js
+++ utility/welcome.js
@@ -3,28 +3,21 @@ const request = require('request');
 module.exports = (req, res) => {
 	if(req.body.queryResult.intent.displayName === "Default Welcome Intent"){
 		res.json({
-			"fulfillmentMessages": [{
+			"fulfillmentMessages": [
+			{
 				"quickReplies": {
-					  "title": `Hi! I am James, let's talk about IFRS? 😃.
-					  Click on the buttons below to begin a conversation
-					  `,
+					  "title": `Hi! I am James, let's talk about IFRS? 😃. 
+					  Click on the buttons below to begin a conversation`,
+					  
 					  "quickReplies": [
 					    "The History of IFRS",
 					    "Conceptul Framework",
 					    "IAS",
 					    "IFRS"
 					]
-				},
-				"quickReplies": {
-					  "title": `Hope your good?`,
-					  "quickReplies": [
-					    "The History of IFRS",
-					    "Conceptul Framework",
-					    "IAS",
-					    "IFRS"
-					]
-				}				
-			}]			
+				}
+			}
+			]			
 		})
 	}
 }
