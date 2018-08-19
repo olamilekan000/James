7606e4646ddabf1702aa158f715ec4de480f6c29
Olamilekan Odukoya <olarpringo@gmail.com>
2018-08-19 03:09:48 +0100

    IAS 2 overview

--- standards/IASs/IAS2/IAS2.js
+++ standards/IASs/IAS2/IAS2.js
@@ -3,21 +3,31 @@ const texts = require('./ias2Texts');
 module.exports = (req, res) => {
 	
 	switch(req.body.queryResult.intent.displayName){
-		case req.body.queryResult.intent.displayName === "IAS2":
-		res.json({
-			"fulfillmentMessages": [{
-				"quickReplies": {
-					  "title": texts.overview,
-					  "quickReplies": [
-					    "Objective",
-					    "Scope",
-					    "Home 🏠"
-					]
-				}
-			}]
-		})
-		break
-
+		case "IAS2":
+			res.json({
+				"fulfillmentMessages": [{
+					"quickReplies": {
+						  "title": texts.overview,
+						  "quickReplies": [
+						    "Objective",
+						    "Scope",
+						    "Home 🏠"
+						]
+					}
+				}]
+			});
+			break;
+		default:
+			res.json({
+				"fulfillmentMessages": [{
+					"quickReplies": {
+						  "title": "Still learning...",
+						  "quickReplies": [
+						    "Home 🏠"
+						]
+					}
+				}]
+			});
 	}
 }
 
