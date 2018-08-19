e98a7f479bf8babe9f89385438f3dcafca3b1ddf
Olamilekan Odukoya <olarpringo@gmail.com>
2018-08-19 14:50:11 +0100

    presentation ias7

--- standards/IASs/IAS7/ias7Texts.js
+++ standards/IASs/IAS7/ias7Texts.js
@@ -24,42 +24,43 @@ Key principles specified by IAS 7 for the preparation of a statement of cash flo
 (vi) -  for operating cash flows, the direct method of presentation is encouraged, but the indirect method is acceptable [IAS 7.18] 
 
 The direct method shows each major class of gross cash receipts and gross cash payments. The operating cash flows section of the statement of cash flows under the direct method would appear something like this:
-___________________________________________________________
-| Cash receipts from customers					| xx | xxx |
-| Cash paid to suppliers						| xx | xxx |
-| Cash paid to employees						| xx | xxx |
-| Cash paid for other operating expenses		| xx | xxx |
-| Interest paid									| xx | xxx |
-| Income taxes paid								| xx | xxx |
-___________________________________________________________
-| Net cash from operating activities			| xx | xxx |
-___________________________________________________________
+__________________________________________
+| Cash receipts from customers		| xx |
+| Cash paid to suppliers			| xx |
+| Cash paid to employees			| xx |
+| Cash paid for other operating 
+| expenses.							| xx |
+| Interest paid						| xx |
+| Income taxes paid					| xx |
+__________________________________________
+| Net cash from operating activities| xx |
+_________________________________________
 
 The indirect method adjusts accrual basis net profit or loss for the effects of non-cash transactions. The operating cash flows section of the statement of cash flows under the indirect method would appear something like this:
-___________________________________________________________
-| Profit before interest and income taxes		|	 | xx |
-| Add back depreciation							|	 | xx | 
-| Add back impairment of assets					|	 | xx |
-| Increase in receivables						|	 | xx |
-| Decrease in inventories				 		|	 | xx |
-| Increase in trade payables					|	 | xx |
-| Interest expense								| xx |	  |	
-| Less Interest accrued but not yet paid		| xx |    |	
-| Interest paid									|	 | xx |
-| Income taxes paid								|	 | xx |
-| ________________________________________________________
-| Net cash from operating activities			|	 | xx |
-___________________________________________________________
+_________________________________________________
+| Profit before interest and income taxes		|
+| Add back depreciation							| 
+| Add back impairment of assets					|
+| Increase in receivables						|
+| Decrease in inventories				 		|
+| Increase in trade payables					|
+| Interest expense								|
+| Less Interest accrued but not yet paid		|
+| Interest paid									|
+| Income taxes paid								|
+| _______________________________________________
+| Net cash from operating activities			|
+_________________________________________________
 
 (i) - the exchange rate used for translation of transactions denominated in a foreign currency should be the rate in effect at the date of the cash flows 
 (ii) - cash flows of foreign subsidiaries should be translated at the exchange rates prevailing when the cash flows took place
 (iii) - as regards the cash flows of associates, joint ventures, and subsidiaries, where the equity or cost method is used, the statement of cash flows should report only cash flows between the investor and the investee; where proportionate consolidation is used, the cash flow statement should include the venturer's share of the cash flows of the investee
 (iV) - aggregate cash flows relating to acquisitions and disposals of subsidiaries and other business units should be presented separately and classified as investing activities, with specified additional disclosures. The aggregate cash paid or received as consideration should be reported net of cash and cash equivalents acquired or disposed of
 (v) - cash flows from investing and financing activities should be reported gross by major class of cash receipts and major class of cash payments except for the following cases, which may be reported on a net basis:
-		=> cash receipts and payments on behalf of customers (for example, receipt and repayment of demand deposits by banks, and receipts collected on behalf of and paid over to the owner of a property) 
-		=> cash receipts and payments for items in which the turnover is quick, the amounts are large, and the maturities are short, generally less than three months (for example, charges and collections from credit card customers, and purchase and sale of investments) 
-		=> cash receipts and payments relating to deposits by financial institutions 
-		=> cash advances and loans made to customers and repayments thereof
+	=> cash receipts and payments on behalf of customers (for example, receipt and repayment of demand deposits by banks, and receipts collected on behalf of and paid over to the owner of a property) 
+	=> cash receipts and payments for items in which the turnover is quick, the amounts are large, and the maturities are short, generally less than three months (for example, charges and collections from credit card customers, and purchase and sale of investments) 
+	=> cash receipts and payments relating to deposits by financial institutions 
+	=> cash advances and loans made to customers and repayments thereof
 (vi) - investing and financing transactions which do not require the use of cash should be excluded from the statement of cash flows, but they should be separately disclosed elsewhere in the financial statements
 (vii) - entities shall provide disclosures that enable users of financial statements to evaluate changes in liabilities arising from financing activities 
 (viii) - the components of cash and cash equivalents should be disclosed, and a reconciliation presented to amounts reported in the statement of financial position
