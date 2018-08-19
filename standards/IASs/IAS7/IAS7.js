c9e9c410d5cedc73e3dadeda19d7665a3ff80d75
Olamilekan Odukoya <olarpringo@gmail.com>
2018-08-19 13:46:00 +0100

    ias7

--- standards/IASs/IAS7/IAS7.js
+++ standards/IASs/IAS7/IAS7.js
@@ -2,61 +2,8 @@ const texts = require('./ias7Texts');
 
 module.exports = (req, res) => {
 	
-	// switch(req.body.queryResult.intent.displayName){
-	// 	case "IAS7":
-	// 		res.json({
-	// 			"fulfillmentMessages": [{
-	// 				"quickReplies": {
-	// 					  "title": texts.overview,
-	// 					  "quickReplies": [
-	// 					    "Objective",
-	// 					    "measurement",
-	// 					    "Home 🏠"
-	// 					]
-	// 				}
-	// 			}]
-	// 		});
-	// 		break;
-	// 	case "IAS7 - Objective":
-	// 		res.json({
-	// 			"fulfillmentMessages": [{
-	// 				"quickReplies": {
-	// 					  "title": texts.obj,
-	// 					  "quickReplies": [
-	// 					  	"IAS7",
-	// 					    "Home 🏠"
-	// 					]
-	// 				}
-	// 			}]
-	// 		});
-	// 		break;	
-	// 	case "IAS7 - Presentation of the Statement of Cash Flows":
-	// 		res.json({
-	// 			"fulfillmentMessages": [{
-	// 				"quickReplies": {
-	// 					  "title": texts.presentation,
-	// 					  "quickReplies": [
-	// 						"IAS7",
-	// 					    "Home 🏠"
-	// 					]
-	// 				}
-	// 			}]
-	// 		});
-	// 		break;																												
-	// 	default:
-	// 		res.json({
-	// 			"fulfillmentMessages": [{
-	// 				"quickReplies": {
-	// 					  "title": "oops, I haven't learnt that... 🙈",
-	// 					  "quickReplies": [
-	// 					  	"IAS7",
-	// 					    "Home 🏠"
-	// 					]
-	// 				}
-	// 			}]
-	// 		});
-	// }
-	if (req.body.queryResult.intent.displayName === "IAS7") {
+	switch(req.body.queryResult.intent.displayName){
+		case "IAS7":
 			res.json({
 				"fulfillmentMessages": [{
 					"quickReplies": {
@@ -68,8 +15,46 @@ module.exports = (req, res) => {
 						]
 					}
 				}]
-			});		
-
+			});
+			break;
+		case "IAS7 - Objective":
+			res.json({
+				"fulfillmentMessages": [{
+					"quickReplies": {
+						  "title": texts.obj,
+						  "quickReplies": [
+						  	"IAS7",
+						    "Home 🏠"
+						]
+					}
+				}]
+			});
+			break;	
+		case "IAS7 - Presentation of the Statement of Cash Flows":
+			res.json({
+				"fulfillmentMessages": [{
+					"quickReplies": {
+						  "title": texts.presentation,
+						  "quickReplies": [
+							"IAS7",
+						    "Home 🏠"
+						]
+					}
+				}]
+			});
+			break;																												
+		default:
+			res.json({
+				"fulfillmentMessages": [{
+					"quickReplies": {
+						  "title": "oops, I haven't learnt that... 🙈",
+						  "quickReplies": [
+						  	"IAS7",
+						    "Home 🏠"
+						]
+					}
+				}]
+			});
 	}
 }
 
