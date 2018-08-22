e7a99231986641d27c8415706a3600ede8b7e079
Olamilekan Odukoya <olarpringo@gmail.com>
2018-08-22 19:59:38 +0100

    added IAS8

--- standards/IASs/IAS8/IAS8.js
+++ standards/IASs/IAS8/IAS8.js
@@ -1,34 +1,19 @@
 const IAS8texts = require('./IAS8texts')
 
 module.exports = (req, res) => {
-	
-	switch(req.body.queryResult.intent.displayName){
-		case "IAS8":
-			res.json({
-				"fulfillmentMessages": [{
-					"quickReplies": {
-						  "title": "IAS8texts.overview",
-						  "quickReplies": [
-						    "Objective",
-						    "disclosure",
-						    "Scope",
-						    "Home 🏠"
-						]
-					}
-				}]
-			});
-			break;
-		default:
-			res.json({
-				"fulfillmentMessages": [{
-					"quickReplies": {
-						  "title": "oops, I haven't learnt IAS8... 🙈",
-						  "quickReplies": [
-						  	"IAS",
-						    "Home 🏠"
-						]
-					}
-				}]
-			});			
-	}
+	if (req.body.queryResult.intent.displayName === "IAS8") {
+		res.json({
+			"fulfillmentMessages": [{
+				"quickReplies": {
+					  "title": IAS8texts.overview,
+					  "quickReplies": [
+					    "Objective",
+					    "disclosure",
+					    "Scope",
+					    "Home 🏠"
+					]
+				}
+			}]
+		});		
+	}	
 }
