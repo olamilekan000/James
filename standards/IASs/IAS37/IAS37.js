a87a7048c0c4e8c70fa3178a89913912384f184b
Olamilekan Odukoya <olarpringo@gmail.com>
2018-09-10 12:27:54 +0100

    timeout error

--- standards/IASs/IAS37/IAS37.js
+++ standards/IASs/IAS37/IAS37.js
@@ -231,6 +231,266 @@ module.exports = (req, res) => {
 				}	
 			}
 		})
-	}						
+	}	
+	if (req.body.queryResult.intent.displayName === "IAS 37 - Measurement of Provisions") {
+		return res.json({
+			"fulfillmentMessages": [{
+				"quickReplies": {
+					  "title": texts.MeasurementProvisions,
+					  "quickReplies": [
+					    "IAS 37",
+					    "Home 🏠"
+					]
+				}
+			}],
+			"payload": {
+			    "google": {
+			      "expectUserResponse": true,
+			      "richResponse": {
+				        "items": [{
+					        "simpleResponse": {
+					            "textToSpeech": `${texts.MeasurementProvisions} \n What else do you want to hear about?`,
+					        	}
+				        	}],
+				        "suggestions": [
+			        		{
+			        			"title": "Home"
+			        		},
+			        		{
+			        			"title": "IAS 37"
+			        		}
+				       	]
+			    	}
+				}	
+			}
+		})
+	}
+	if (req.body.queryResult.intent.displayName === "IAS 37 - Remeasurement of Provison") {
+		return res.json({
+			"fulfillmentMessages": [{
+				"quickReplies": {
+					  "title": texts.RemeasurementProvison,
+					  "quickReplies": [
+					    "IAS 37",
+					    "Home 🏠"
+					]
+				}
+			}],
+			"payload": {
+			    "google": {
+			      "expectUserResponse": true,
+			      "richResponse": {
+				        "items": [{
+					        "simpleResponse": {
+					            "textToSpeech": `${texts.RemeasurementProvison} \n What else do you want to hear about?`,
+					        	}
+				        	}],
+				        "suggestions": [
+			        		{
+			        			"title": "Home"
+			        		},
+			        		{
+			        			"title": "IAS 37"
+			        		}
+				       	]
+			    	}
+				}	
+			}
+		})
+	}
+	if (req.body.queryResult.intent.displayName === "IAS 37 - Restructuring") {
+		return res.json({
+			"fulfillmentMessages": [{
+				"quickReplies": {
+					  "title": texts.Restructurings,
+					  "quickReplies": [
+					    "IAS 37",
+					    "Home 🏠"
+					]
+				}
+			}],
+			"payload": {
+			    "google": {
+			      "expectUserResponse": true,
+			      "richResponse": {
+				        "items": [{
+					        "simpleResponse": {
+					            "textToSpeech": `${texts.Restructurings} \n What else do you want to hear about?`,
+					        	}
+				        	}],
+				        "suggestions": [
+			        		{
+			        			"title": "Home"
+			        		},
+			        		{
+			        			"title": "IAS 37"
+			        		}
+				       	]
+			    	}
+				}	
+			}
+		})
+	}
+	if (req.body.queryResult.intent.displayName === "IAS 37 - restructuring Provison") {
+		return res.json({
+			"fulfillmentMessages": [{
+				"quickReplies": {
+					  "title": texts.restructuringProvison,
+					  "quickReplies": [
+					    "IAS 37",
+					    "Home 🏠"
+					]
+				}
+			}],
+			"payload": {
+			    "google": {
+			      "expectUserResponse": true,
+			      "richResponse": {
+				        "items": [{
+					        "simpleResponse": {
+					            "textToSpeech": `${texts.restructuringProvison} \n What else do you want to hear about?`,
+					        	}
+				        	}],
+				        "suggestions": [
+			        		{
+			        			"title": "Home"
+			        		},
+			        		{
+			        			"title": "IAS 37"
+			        		}
+				       	]
+			    	}
+				}	
+			}
+		})
+	}
+	if (req.body.queryResult.intent.displayName === "IAS 37 - debit Entry") {
+		return res.json({
+			"fulfillmentMessages": [{
+				"quickReplies": {
+					  "title": texts.debitEntry,
+					  "quickReplies": [
+					    "IAS 37",
+					    "Home 🏠"
+					]
+				}
+			}],
+			"payload": {
+			    "google": {
+			      "expectUserResponse": true,
+			      "richResponse": {
+				        "items": [{
+					        "simpleResponse": {
+					            "textToSpeech": `${texts.debitEntry} \n What else do you want to hear about?`,
+					        	}
+				        	}],
+				        "suggestions": [
+			        		{
+			        			"title": "Home"
+			        		},
+			        		{
+			        			"title": "IAS 37"
+			        		}
+				       	]
+			    	}
+				}	
+			}
+		})
+	}
+	if (req.body.queryResult.intent.displayName === "IAS 37 - use provisions") {
+		return res.json({
+			"fulfillmentMessages": [{
+				"quickReplies": {
+					  "title": texts.useprovisions,
+					  "quickReplies": [
+					    "IAS 37",
+					    "Home 🏠"
+					]
+				}
+			}],
+			"payload": {
+			    "google": {
+			      "expectUserResponse": true,
+			      "richResponse": {
+				        "items": [{
+					        "simpleResponse": {
+					            "textToSpeech": `${texts.useprovisions} \n What else do you want to hear about?`,
+					        	}
+				        	}],
+				        "suggestions": [
+			        		{
+			        			"title": "Home"
+			        		},
+			        		{
+			        			"title": "IAS 37"
+			        		}
+				       	]
+			    	}
+				}	
+			}
+		})
+	}
+	if (req.body.queryResult.intent.displayName === "IAS 37 - Disclosures") {
+		return res.json({
+			"fulfillmentMessages": [{
+				"quickReplies": {
+					  "title": texts.Disclosures,
+					  "quickReplies": [
+					    "IAS 37",
+					    "Home 🏠"
+					]
+				}
+			}],
+			"payload": {
+			    "google": {
+			      "expectUserResponse": true,
+			      "richResponse": {
+				        "items": [{
+					        "simpleResponse": {
+					            "textToSpeech": `${texts.Disclosures} \n What else do you want to hear about?`,
+					        	}
+				        	}],
+				        "suggestions": [
+			        		{
+			        			"title": "Home"
+			        		},
+			        		{
+			        			"title": "IAS 37"
+			        		}
+				       	]
+			    	}
+				}	
+			}
+		})
+	}
+	if ( req.body.webhookStatus.message === "Webhook call failed. Error: Request timeout.") {
+		return res.json({
+			"fulfillmentMessages": [{
+				"quickReplies": {
+					  "title": "I could not process that... I'll get back to you soon",
+					  "quickReplies": [
+					    "Home 🏠"
+					]
+				}
+			}],
+			"payload": {
+			    "google": {
+			      "expectUserResponse": true,
+			      "richResponse": {
+				        "items": [{
+					        "simpleResponse": {
+					            "textToSpeech": "I could not process that... I'll get back to you soon",
+					        	}
+				        	}],
+				        "suggestions": [
+			        		{
+			        			"title": "Home"
+			        		}
+				       	]
+			    	}
+				}	
+			}			
+		})
+	}												
 }
 	
