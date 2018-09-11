a260366ce6cdf13a851c224e6151bf262b51c8cf
Olamilekan Odukoya <olarpringo@gmail.com>
2018-09-11 12:01:00 +0100

    IFRS functions

--- appFunctions.js
+++ appFunctions.js
@@ -24,6 +24,23 @@ const IAS37 = require('./standards/IASs/IAS37/IAS37');//for ias-20
 const IAS38 = require('./standards/IASs/IAS38/IAS38');//for ias-20
 const IAS40 = require('./standards/IASs/IAS40/IAS40');//for ias-20
 const IAS41 = require('./standards/IASs/IAS41/IAS41');//for ias-20
+const IFRS1 = require('./standards/IFRSs/IFRS1/IFRS1');//for IFRS 1
+const IFRS2 = require('./standards/IFRSs/IFRS2/IFRS2');//for IFRS 2
+const IFRS3 = require('./standards/IFRSs/IFRS3/IFRS3');//for IFRS 3
+const IFRS4 = require('./standards/IFRSs/IFRS4/IFRS4');//for IFRS 4
+const IFRS5 = require('./standards/IFRSs/IFRS5/IFRS5');//for IFRS 5
+const IFRS6 = require('./standards/IFRSs/IFRS6/IFRS6');//for IFRS 6
+const IFRS7 = require('./standards/IFRSs/IFRS7/IFRS7');//for IFRS 7
+const IFRS8 = require('./standards/IFRSs/IFRS8/IFRS8');//for IFRS 8
+const IFRS9 = require('./standards/IFRSs/IFRS9/IFRS9');//for IFRS 9
+const IFRS10 = require('./standards/IFRSs/IFRS10/IFRS10');//for IFRS 10
+const IFRS11 = require('./standards/IFRSs/IFRS11/IFRS11');//for IFRS 11
+const IFRS12 = require('./standards/IFRSs/IFRS12/IFRS12');//for IFRS 12
+const IFRS13 = require('./standards/IFRSs/IFRS13/IFRS13');//for IFRS 13
+const IFRS14 = require('./standards/IFRSs/IFRS14/IFRS14');//for IFRS 14
+const IFRS15 = require('./standards/IFRSs/IFRS15/IFRS15');//for IFRS 15
+const IFRS16 = require('./standards/IFRSs/IFRS16/IFRS16');//for IFRS 16
+const IFRS17 = require('./standards/IFRSs/IFRS17/IFRS17');//for IFRS 17
 
 module.exports = (req, res) => {
 	//individual standards
@@ -53,4 +70,22 @@ module.exports = (req, res) => {
 	IAS38(req, res);
 	IAS40(req, res);
 	IAS41(req, res);
+	IFRS1(req, res);
+	IFRS2(req, res);
+	IFRS3(req, res);
+	IFRS4(req, res);
+	IFRS5(req, res);
+	IFRS6(req, res);
+	IFRS7(req, res);
+	IFRS8(req, res);
+	IFRS9(req, res);
+	IFRS10(req, res);
+	IFRS11(req, res);
+	IFRS12(req, res);
+	IFRS13(req, res);
+	IFRS14(req, res);
+	IFRS15(req, res);
+	IFRS16(req, res);
+	IFRS17(req, res);
+
 }
---
+++ standards/IFRSs/IFRS1/IFRS1.js
@@ -0,0 +1,103 @@
+const texts = require('./IFRS1texts')
+
+module.exports = (req, res) => {
+	if (req.body.queryResult.intent.displayName === "IFRS 1") {
+		return res.json({
+			"fulfillmentMessages": [{
+				"quickReplies": {
+					  "title": texts.overview,
+					  "quickReplies": [
+					    "Objective Of IFRS 1",	    
+					    "Home 🏠"
+					]
+				}
+			}],
+			"payload": {
+			    "google": {
+			      "expectUserResponse": true,
+			      "richResponse": {
+				        "items": [{
+					        "simpleResponse": {
+					            "textToSpeech": texts.overview,
+					        	}
+				        	}],
+				        "suggestions": [
+			        		{
+			        			"title": "Home 🏠"
+			        		},
+			        		{
+			        			"title": "Objective Of IFRS 1"
+			        		}
+				       	]
+			    	}
+				}	
+			}
+		});		
+	}
+	if (req.body.queryResult.intent.displayName === "IFRS 1 - Objective") {
+		return res.json({
+			"fulfillmentMessages": [{
+				"quickReplies": {
+					  "title": texts.obj,
+					  "quickReplies": [
+					    "IFRS 1",	    
+					    "Home 🏠"
+					]
+				}
+			}],
+			"payload": {
+			    "google": {
+			      "expectUserResponse": true,
+			      "richResponse": {
+				        "items": [{
+					        "simpleResponse": {
+					            "textToSpeech": texts.obj,
+					        	}
+				        	}],
+				        "suggestions": [
+			        		{
+			        			"title": "Home 🏠"
+			        		},
+			        		{
+			        			"title": "IFRS 1"
+			        		}
+				       	]
+			    	}
+				}	
+			}
+		});		
+	}
+	if (req.body.queryResult.intent.displayName === "IFRS 1 - definition Of First Time Adoption") {
+		return res.json({
+			"fulfillmentMessages": [{
+				"quickReplies": {
+					  "title": texts.defOfFirstTimeAdoption,
+					  "quickReplies": [
+					    "IFRS 1",	    
+					    "Home 🏠"
+					]
+				}
+			}],
+			"payload": {
+			    "google": {
+			      "expectUserResponse": true,
+			      "richResponse": {
+				        "items": [{
+					        "simpleResponse": {
+					            "textToSpeech": texts.defOfFirstTimeAdoption,
+					        	}
+				        	}],
+				        "suggestions": [
+			        		{
+			        			"title": "Home 🏠"
+			        		},
+			        		{
+			        			"title": "IFRS 1"
+			        		}
+				       	]
+			    	}
+				}	
+			}
+		});		
+	}			
+}
